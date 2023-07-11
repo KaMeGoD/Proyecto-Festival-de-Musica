279efb8b71715cec73e8c2dbc7eb83c65743c197
KaMeGoD <jcgarciadev@gmail.com>
2023-07-10 23:56:59 -0400

    adasda

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,29 @@
 import React, { useEffect } from 'react';
 
-export const Header: React.FC = () => {
-
+const Header: React.FC = () => {
   const scrollNav = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     const seccionScroll = e.currentTarget.getAttribute("href");
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const seccion = document.querySelector(seccionScroll!);
     seccion?.scrollIntoView({ behavior: "smooth" });
   }
 
   useEffect(() => {
-    function navegacionFija() {
+    const navegacionFija = () => {
       const barra = document.querySelector('header');
       const sobreFestival = document.querySelector('.sobre-festival');
       const body = document.querySelector('body');
-      const bbd: number | undefined =  sobreFestival?.getBoundingClientRect().bottom 
 
       const handleScroll = () => {
-        if ( bbd < 0) {
-          barra?.classList.add('fijo');
-          body?.classList.add('body-scroll');
-        } else {
-          barra?.classList.remove('fijo');
-          body?.classList.remove('body-scroll');
+        if (sobreFestival) {
+          const boundingRect = sobreFestival.getBoundingClientRect();
+          if (boundingRect.bottom < 0) {
+            barra?.classList.add('fijo');
+            body?.classList.add('body-scroll');
+          } else {
+            barra?.classList.remove('fijo');
+            body?.classList.remove('body-scroll');
+          }
         }
       };
 
@@ -33,13 +33,14 @@ export const Header: React.FC = () => {
         window.removeEventListener('scroll', handleScroll);
       };
     }
+
     navegacionFija();
   }, []);
 
   return (
     <header className="header">
       <div className="contenedor contenido-header">
-        <h1>Rock &amp; EDM Fest</h1>
+        <h1>Rock & EDM Fest</h1>
         <nav className="navegacion-principal">
           <a href="#lineup" onClick={scrollNav}>Line Up</a>
           <a href="#galeria" onClick={scrollNav}>Galeria</a>
@@ -49,3 +50,5 @@ export const Header: React.FC = () => {
     </header>
   );
 }
+
+export default Header;
