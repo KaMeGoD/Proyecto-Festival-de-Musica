7e439c005b733aec11b89dadc116e09bb52989ef
KaMeGoD <jcgarciadev@gmail.com>
2023-07-10 23:50:08 -0400

    arreglando cositas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,51 @@
+import React, { useEffect } from 'react';
 
-export const Header = () => {
+export const Header: React.FC = () => {
 
-  const scrollNav = (e: React.MouseEvent)=>{
+  const scrollNav = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
-    const seccionScroll:string | null = e.currentTarget.getAttribute("href");
-    const seccion = document.querySelector(seccionScroll);
-    seccion?.scrollIntoView({behavior: "smooth"});
+    const seccionScroll = e.currentTarget.getAttribute("href");
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const seccion = document.querySelector(seccionScroll!);
+    seccion?.scrollIntoView({ behavior: "smooth" });
   }
 
-  function navegacionFija(){
-    const barra = document.querySelector('header');
-    const sobreFestival = document.querySelector('.sobre-festival');
-    const body = document.querySelector('body');
-    window.addEventListener('scroll', function(){
-      // console.log(sobreFestival?.getBoundingClientRect());
-      if(sobreFestival?.getBoundingClientRect().bottom < 0){
-        barra?.classList.add('fijo');
-        body?.classList.add('body-scroll');
-      }else{
-        barra?.classList.remove('fijo');
-        body?.classList.remove('body-scroll');
-      }
-      }
-    )}
-    navegacionFija();
+  useEffect(() => {
+    function navegacionFija() {
+      const barra = document.querySelector('header');
+      const sobreFestival = document.querySelector('.sobre-festival');
+      const body = document.querySelector('body');
+      const bbd: number | undefined =  sobreFestival?.getBoundingClientRect().bottom 
 
+      const handleScroll = () => {
+        if ( bbd < 0) {
+          barra?.classList.add('fijo');
+          body?.classList.add('body-scroll');
+        } else {
+          barra?.classList.remove('fijo');
+          body?.classList.remove('body-scroll');
+        }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
+    navegacionFija();
+  }, []);
 
   return (
-    <>
-      <header className="header">
-        <div className="contenedor contenido-header">
-          <h1>Rock & EDM Fest</h1>
-          <nav className="navegacion-principal">
-            <a href="#lineup" onClick={scrollNav}>Line Up</a>
-            <a href="#galeria" onClick={scrollNav} >Galeria</a>
-            <a href="#boletos" onClick={scrollNav} >Boletos</a>
-          </nav>
-        </div>
-      </header>
-    </>
+    <header className="header">
+      <div className="contenedor contenido-header">
+        <h1>Rock &amp; EDM Fest</h1>
+        <nav className="navegacion-principal">
+          <a href="#lineup" onClick={scrollNav}>Line Up</a>
+          <a href="#galeria" onClick={scrollNav}>Galeria</a>
+          <a href="#boletos" onClick={scrollNav}>Boletos</a>
+        </nav>
+      </div>
+    </header>
   );
 }
